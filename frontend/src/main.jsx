4a6f499a7e4937c8b3691f1f9e815d9c4188681f
k1rvl_ <kirkorol07@gmail.com>
2025-03-15 20:18:26 +0300

    fix: providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
-import { ReactQueryProvider } from "./react-query.jsx";
+import { ReactQueryProvider } from "./providers/ReactQuerry.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
