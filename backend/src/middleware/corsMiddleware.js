0d06f3ae33b578d9e7256b3240914669429c3cce
k1rvl_ <kirkorol07@gmail.com>
2025-03-18 00:33:59 +0300

    fix: cors for GitHubPages

--- backend/src/middleware/corsMiddleware.js
+++ backend/src/middleware/corsMiddleware.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: "http://localhost:3000" || "http://localhost:5173" || "https://k1rvl07.github.io",
+  origin: ["http://localhost:3000", "http://localhost:5173", "https://k1rvl07.github.io"],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
